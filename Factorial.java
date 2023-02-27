c71e326527b0c6d0a6a72df4f6d1b219e40611d7
kajalkhirodkar <vaishnavikulkarni2210@gmail.com>
2023-02-27 16:52:51 +0530

    Initial Commit for Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Factorial{
+ 
+    // factorial method declaration
+    public static int factorial(int n) {
+        if(n < 0){
+            System.out.println("Error: n must be positive");
+            return 0;
+        }
+        if(n==0)
+        {
+            return 1;
+        }
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+    }
+    
+    // main method
+    public static void main(String[] args) {
+        // variable declaration and initialization
+        int n;
+
+        // object declaration and initialization
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        n = sc.nextInt();
+        System.out.println("Factorial of " + n + " is: " + factorial(n));
+
+        sc.close();
+    }
+}
