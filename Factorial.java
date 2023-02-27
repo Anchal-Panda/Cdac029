cc1aef1a3612435e94f6bbbd9235513756e8b403
Anchal-Panda <anchal.p@outlook.com>
2023-02-27 18:22:19 +0530

    final

--- Factorial.java
+++ Factorial.java
@@ -22,7 +22,7 @@ public class Factorial{
     // main method
     public static void main(String[] args) {
         // variable declaration and initialization
-        int n;
+        int n=10;
 
         // object declaration and initialization
         Scanner sc = new Scanner(System.in);
